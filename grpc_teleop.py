aee90271acba0e6af97d1b5e36a2ffa7aefb063c
wenlinfeng <wenlinfeng@xiaomi.com>
2023-08-08 11:38:44 +0800

    [Modify] Add SSL/TLS authentication to gRPC
    1.给gRPC添加SSL/TLS认证
    
    Signed-off-by: wenlinfeng <wenlinfeng@xiaomi.com>

--- grpc_teleop.py
+++ grpc_teleop.py
@@ -16,17 +16,26 @@ import cyberdog_app_pb2
 import cyberdog_app_pb2_grpc
 
 class Client:
-    def __init__(self, cyberdog_ip: str):
-        chennel = grpc.insecure_channel(cyberdog_ip + ':50052')
+    def __init__(self, cyberdog_ip: str, ca_cert: str, client_key: str, client_cert: str):
+        creds = grpc.ssl_channel_credentials(
+            open(ca_cert, 'rb').read(),
+            open(client_key, 'rb').read(),
+            open(client_cert, 'rb').read())
+        channel_options = (('grpc.ssl_target_name_override', 'cyberdog2.server'), 
+                           ('grpc.default_authority', 'cyberdog2.server'))
+        chennel = grpc.secure_channel(cyberdog_ip + ':50052', creds, channel_options)
         self.__stub = cyberdog_app_pb2_grpc.GrpcAppStub(chennel)
         print('Client is ready.')
 
     def sendMsg(self, name_code, params):
-        result_list = self.__stub.sendMsg(
-            cyberdog_app_pb2.SendRequest(
-                nameCode=name_code,
-                params=params))
-        resp = list(result_list)
+        try:
+            result_list = self.__stub.sendMsg(
+                cyberdog_app_pb2.SendRequest(
+                    nameCode=name_code,
+                    params=params))
+            resp = list(result_list)
+        except:
+            print('failed to send msg')
 
 class Teleop:
     def __init__(self, acc=[0.1, 0.0, 1.0], freq=10.0, max_vel=[1.0, 0.0, 1.5]):
@@ -107,10 +116,10 @@ class ProtoEncoder:
         return json.dumps(cmd)
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print('Please input gRPC server IP')
+    if len(sys.argv) < 5:
+        print('Please input gRPC server IP, CA certificate, client key and client certificate')
         exit()
-    grpc_client = Client(sys.argv[1])
+    grpc_client = Client(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
     teleop = Teleop()
     encoder = ProtoEncoder()
     stop_signal = False
